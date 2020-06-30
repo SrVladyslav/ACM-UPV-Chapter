91121559dda51695ecbfd4ddb5865b679a34d0f5
SrVladyslav <vlama@inf.upv.es>
2020-06-30 16:18:32 +0200

    toggle

--- js/navbar.js
+++ js/navbar.js
@@ -2,26 +2,15 @@
 $(document).ready(function() {
     if ($(document).scrollTop() <= 100) {
         $('#nav').addClass('navbar_top');
-        //$('#navbar').removeClass('barra');+
-        //$('.navbar').css({"background-color":"white!important;"});
     } else {
         $('#nav').removeClass('navbar_top');
-        //$('#navBar').addClass('barra');
-        //$('#navBar').removeClass('header_color');
-        //$('.navbar').css({"background-color":"black!important;"});
     }
 });
 $(window).scroll(function() {
     if ($(document).scrollTop() <= 100) {
         $('#nav').addClass('navbar_top');
-        //$('.navbar').css({"background-color":"white!important;"});
-        //$('#navBar').addClass('header_color');
-        //$('#navBar').removeClass('barra');
     } else {
         $('#nav').removeClass('navbar_top');
-        //$('.navbar').css({"background-color":"black!important;"});
-        //$('#navBar').addClass('barra');
-        //$('#navBar').removeClass('header_color');
     }
 });
 
@@ -30,7 +19,7 @@ const navSlide = () => {
     const burger= document.querySelector('.burger');
     const navbar= document.querySelector('.navbar');
 	const nav = document.querySelector('.nav_links');
-	const navLinks = document.querySelectorAll('.nav_link a');
+	const navLinks = document.querySelectorAll('.nav_links');
 	//Toggle nav
 	burger.addEventListener('click', () => {
 		navbar.classList.toggle('nav_active');
@@ -40,18 +29,21 @@ const navSlide = () => {
 			if(link.style.animation){
 				link.style.animation = '';
 			} else {
-				link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+				link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s';
 			}
 		});
 		//Burger animation
 		burger.classList.toggle('toggle');
     });
     
+
     nav.addEventListener('click', ()=> {
-        console.log("Pulsado");
-        navbar.classList.toggle('nav_active');
-        //Burger animation
-		burger.classList.toggle('toggle');
+        if($(".burger").hasClass("nav_active")) {
+            console.log("Pulsado");
+            navbar.classList.toggle('nav_active');
+            //Burger animation
+            burger.classList.toggle('toggle');
+        }
     });
 	
 }
